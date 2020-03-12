aa2c9bef631b5e2e6dcfe90d96e3089701dd720c
Marek Belisko <marek.belisko@open-nandra.com>
2020-03-12 09:14:38 +0100

    Use kas-docker
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- build_yocto.sh
+++ build_yocto.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run -it -e USER_ID=$(id -u) --rm -v"`pwd`:/home/builder" -w "/home/builder" kasproject/kas:latest  sh -c "kas build poky-thud-test.yml"
+./kas-docker build poky-thud-test.yml
+
+#docker run -it -e USER_ID=$(id -u) --rm -v"`pwd`:/home/builder" -w "/home/builder" kasproject/kas:latest  sh -c "kas build poky-thud-test.yml"
